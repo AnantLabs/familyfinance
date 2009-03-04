9af5ef3f7548452a9f2698265a3f4b64b9a35988
erik.redned <erik.redned@4231dae8-a09e-11dd-8697-036594075916>
2009-03-04 05:53:47 +0000

    Added the registry panel to eventually place the LineItem and SubLineView dataGridViews.

---
+++ Code/FamilyFinance2/Custom Controls/RegistryPanel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FamilyFinance2.Custom_Controls
+{
+    public partial class RegistryPanel : UserControl
+    {
+        public RegistryPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void lineItemBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.lineItemBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.fFDBDataSet);
+
+        }
+    }
+}
