ed754725205bb55b13dd0b915c2d03c42e97d9e0
erik.redned <erik.redned@4231dae8-a09e-11dd-8697-036594075916>
2010-04-04 15:58:16 +0000

    Converting to AccountBrowser

--- trunk/Code/FamilyFinance2/Forms/Main/RegistrySplit/TreeView/AccountItemSorter.cs
+++ trunk/Code/FamilyFinance2/Forms/Main/RegistrySplit/TreeView/AccountItemSorter.cs
@@ -4,39 +4,15 @@ using System.Windows.Forms;
 
 namespace FamilyFinance2.Forms.Main.RegistrySplit.TreeView
 {
-	public class FolderItemSorter : IComparer
+	public class AccountItemSorter : IComparer
 	{
-		private string _mode;
-		private SortOrder _order;
-
-		public FolderItemSorter(string mode, SortOrder order)
+        public AccountItemSorter()
 		{
-			_mode = mode;
-			_order = order;
 		}
 
 		public int Compare(object x, object y)
 		{
-			BaseItem a = x as BaseItem;
-			BaseItem b = y as BaseItem;
-			int res = 0;
-
-			if (_mode == "Date")
-				res = DateTime.Compare(a.Date, b.Date);
-			else if (_mode == "Size")
-			{
-				if (a.Size < b.Size)
-					res = -1;
-				else if (a.Size > b.Size)
-					res = 1;
-			}
-			else
-				res = string.Compare(a.Name, b.Name);
-
-			if (_order == SortOrder.Ascending)
-				return -res;
-			else
-				return res;
+            return String.Compare((x as BaseItem).Name, (y as BaseItem).Name);
 		}
 
 		private string GetData(object x)
