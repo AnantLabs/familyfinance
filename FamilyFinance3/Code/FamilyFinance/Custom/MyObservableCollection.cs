61acb726d593f10fed2872ebc6b167595cb46a11
erik.redned <erik.redned@4231dae8-a09e-11dd-8697-036594075916>
2010-10-28 15:19:27 +0000

    Updating the registry and the account Tabbed browser.

---
+++ FamilyFinance3/Code/FamilyFinance/Custom/MyObservableCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace FamilyFinance.Custom
+{
+    class MyObservableCollection<T> : ObservableCollection<T>
+    {
+
+        public void sort(IComparer<T> comp)
+        {
+
+        }
+
+    }
+}
