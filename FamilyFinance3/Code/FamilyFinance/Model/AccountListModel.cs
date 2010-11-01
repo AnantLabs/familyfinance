d3ee3fa08189fed3957ef33e4b6340a1118150ea
erik.redned <erik.redned@4231dae8-a09e-11dd-8697-036594075916>
2010-11-01 01:13:57 +0000

    Adding the Account List model to make a single account category combo box.

---
+++ FamilyFinance3/Code/FamilyFinance/Model/AccountListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FamilyFinance.Model
+{
+    class AccountListModel
+    {
+
+        public List<IdNameCat> AccountList
+        {
+            get
+            {
+
+            }
+
+        }
+
+        public static reloadAccount
+
+    }
+}
