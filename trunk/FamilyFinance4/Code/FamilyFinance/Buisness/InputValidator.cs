48dcfd849964cc64aaa2f93abd47f8d46a8d2809
erik.redned <erik.redned@4231dae8-a09e-11dd-8697-036594075916>
2011-07-04 01:30:20 +0000

    forgotten stuff.

---
+++ trunk/FamilyFinance4/Code/FamilyFinance/Buisness/InputValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FamilyFinance.Buisness
+{
+    public static class InputValidator
+    {
+        public static void CheckNotNull(object objectToValidate)
+        {
+            if (objectToValidate == null)
+                allertUserToError(objectToValidate, "Null Object");
+        }
+
+        private static void allertUserToError(object problemObject, string errorType)
+        {
+            string nameOfObjectType = problemObject.GetType().FullName;
+            string messageToUser = "Unexpected " + errorType;
+                        
+            System.Windows.MessageBox.Show(messageToUser, 
+                errorType, 
+                System.Windows.MessageBoxButton.OK, 
+                System.Windows.MessageBoxImage.Stop);
+        }
+
+    }
+}
